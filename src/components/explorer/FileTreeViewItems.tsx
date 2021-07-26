4fa9c6d89d6b7086b59df18c36dfa3ebc3923f7a
pelikhan <jhalleux@microsoft.com>
2021-07-26 03:42:04 -0700

    adding treeview items

--- src/components/explorer/FileTreeViewItems.tsx
+++ src/components/explorer/FileTreeViewItems.tsx
@@ -1,26 +1,37 @@
 import React from "react"
+import {
+    FILE_NODE_NAME,
+    FOLDER_NODE_NAME,
+} from "../../../jacdac-ts/src/jdom/constants"
 import { StyledTreeItem } from "../ui/StyledTreeView"
 
 export function FileSystemHandleFileTreeViewItem(props: {
-    handle: FileSystemFileHandle
+    fileHandle: FileSystemFileHandle
 }) {
-    const { handle } = props
-    const { name } = handle
+    const { fileHandle } = props
+    const { name } = fileHandle
     const id = name
 
-    return (
-        <StyledTreeItem nodeId={id} labelText={name} kind={"file"} />
-    )
+    return <StyledTreeItem nodeId={id} labelText={name} kind={FILE_NODE_NAME} />
 }
 
 export function FileSystemHandleDirectoryTreeViewItem(props: {
     handle: FileSystemDirectoryHandle
+    fileHandles?: FileSystemFileHandle[]
 }) {
-    const { handle } = props
+    const { handle, fileHandles } = props
     const { name } = handle
     const id = name
 
     return (
-        <StyledTreeItem nodeId={id} labelText={name} kind={"directory"} />
+        <StyledTreeItem nodeId={id} labelText={name} kind={FOLDER_NODE_NAME}>
+            {fileHandles &&
+                fileHandles?.map(fileHandle => (
+                    <FileSystemHandleFileTreeViewItem
+                        key={fileHandle.name}
+                        fileHandle={fileHandle}
+                    />
+                ))}
+        </StyledTreeItem>
     )
 }
